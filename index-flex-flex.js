dbef3933c25881f270f650621923e469b9de38c7
Medaminekthir <medaminekthirimsara727309207@gmail.com>
2025-09-21 22:29:01 +0200

    Delete index-flex-flex.js

--- index-flex-flex.js
+++
@@ -1,3 +0,0 @@
-const butt=document.querySelector('.btn').addEventListener('click',()=>{
-    document.querySelector('.container').innerHTML+=` <div class="items"><img class="amazon-logo-div-container" src="images/amazon-logo.png" alt=""></div>`
-})
